e3543725bb34877cd380d466bf4065f78c53b8a4
Sarah Mukuti <cesarwrites24@gmail.com>
2022-05-23 00:34:09 +0300

    Add functio to store items into cart

--- Assets/index.js
+++ Assets/index.js
@@ -1,30 +1,64 @@
-const itemList = [
-    {
-        itemId:001,
-        itemName:"Sugar",
-        itemPrice:100
-    },
-    {
-        itemId:002,
-        itemName:"Rice",
-        itemPrice:"120",
-    },
-    {
-        itemId:003,
-        itemName:"Bread",
-        itemPrice:"60",
-    },
-    {
-        itemId:004,
-        itemName:"Milk",
-        itemPrice:"70"
-    },
-{
-    itemId:005,
-    itemName:"Cooking oil",
-    itemPrice:"350"
-}
-];
 
- 
+  var products = {
+    // (A) PRODUCTS LIST
+    list : {
+      1 : { name:"Banana", img:"banana.png", price: 12 },
+      2 : { name:"Cherry", img:"cherry.png", price: 23 },
+      3 : { name:"Ice Cream", img:"icecream.png", price: 54 },
+      4 : { name:"Orange", img:"orange.png", price: 65 },
+      5 : { name:"Strawberry", img:"strawberry.png", price: 34 },
+      6 : { name:"Watermelon", img:"watermelon.png", price: 67 }
+    },
 
+  draw : () => {
+      const wrapper = document.getElementById("poslist");
+
+      for (let pid in products.list) {
+          let p = products.list[pid],
+          pdt = document.createElement("div"),
+          segment;
+
+          pdt.className = "pwrap";
+          pdt.onclick = () => { createImageBitmap.add(pid); };
+          wrapper.appendChild(pdt);
+
+          segment = document.createElement("img");
+          segment.className = "pimg";
+          segment.src = "images/" + p.img;
+          pdt.appendChild(segment);
+
+          segment =  document.createElement("div");
+          segment.className = "pname";
+          segment.innerHTML = p.name;
+          pdt.appendChild(segment);
+
+          segment = document.createElement("div");
+          segment.className = "pprice";
+          segment.innerHTML = "$" + p.price;
+          pdt.appendChild(segment);
+      }
+  }
+};
+
+  window.addEventListener("DOMContentLoaded",products.draw);
+  var cart = {
+      items : {},
+
+      save : () => {
+          localStorage.setItem("cart", JSON.stringify(cart.items));
+      },
+      load : () => {
+          localStorage.getItem("cart")
+          if(cart.items == null) {cart.items = {}}
+          else{ cart.items = JSON.parse(cart.items);}
+      },
+      nuke : () => {
+          cart.items = {}
+          localStorage.removeItem("cart");
+          cart.list();
+      },
+      init : () => {
+          cart.load();
+          cart.list();
+      }
+  }
