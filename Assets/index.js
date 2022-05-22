2b446d92816110249cc1b23e95a712f07f190d37
Sarah Mukuti <cesarwrites24@gmail.com>
2022-05-22 16:55:15 +0300

    Add the search button

---
+++ Assets/index.js
@@ -0,0 +1,30 @@
+const itemList = [
+    {
+        itemId:001,
+        itemName:"Sugar",
+        itemPrice:100
+    },
+    {
+        itemId:002,
+        itemName:"Rice",
+        itemPrice:"120",
+    },
+    {
+        itemId:003,
+        itemName:"Bread",
+        itemPrice:"60",
+    },
+    {
+        itemId:004,
+        itemName:"Milk",
+        itemPrice:"70"
+    },
+{
+    itemId:005,
+    itemName:"Cooking oil",
+    itemPrice:"350"
+}
+];
+
+ 
+
